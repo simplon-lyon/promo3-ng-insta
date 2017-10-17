934bb4d0839a45056611924cb7f7fac0b90a5df3
Jean <demeljean@hotmail.fr>
2017-10-17 14:38:09 +0200

    changed the authService user to a user stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+import { User } from './shared/user';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  currentUser:Observable<User>;
+
+  constructor(private auth:AuthService){}
+
   title = 'app';
+
+  ngOnInit(): void {
+    /**
+     * Côté component, on peut récupérer ou se subscribe
+     * sur le flux de user et la valeur sera donc mise à
+     * jour en temps réel à chaque changement d'état du
+     * user (par exemple si un autre component déclenche
+     * le login ou le logout, ce component sera mis à 
+     * jour)
+     */
+    this.currentUser = this.auth.user
+    
+  }
 }
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,23 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-  user:User;
+  /* Le BehaviorSubject est un type de Subject particulier
+   de RxJs qui a constamment une valeur, ce qui fait
+   que lorsqu'on se subscribe dessus, on recevra la
+   dernière valeur qui a été poussée dans le flux.
+   Ce type de subject a besoin d'une valeur par défaut
+   au moment de son instanciation, on pourra ensuite
+   lui pousser des valeurs avec la méthode .next(value)
+
+  */
+  user:BehaviorSubject<User> = new BehaviorSubject(null);
   urlAPI:string = 'http://localhost:3000/user';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+    //Quand le service est créé, il va vérifier dans le localStorage si un user s'y trouve déjà, et si oui, il le récupère et le pousse dans le flux de user
+    this.user.next(JSON.parse(localStorage.getItem('user')));
+  }
+
 
   login(username:string,pass:string):Observable<boolean> {
     
     return this.http.get<User[]>(this.urlAPI+'?username='+username+'&password='+pass)
     .map((users) => {
       if(users.length === 1) {
-        this.user = users[0];
+        //On stock le user en localStorage (c'est pas 
+        //très très bien de faire ça, en soit on voudrait
+        //plutôt stocker juste une clef token en localStorage et pas tout le user)
+        localStorage.setItem('user', JSON.stringify(users[0]));
+        //On pousse le user dans le flux de User qu'est notre BehaviorSubject
+        this.user.next(users[0]);
         return true;
       }
       return false;
@@ -25,7 +44,9 @@ export class AuthService {
   }
 
   logout():void {
-    this.user = null;
+    //Au logout, on supprime le user des localStorage
+    localStorage.removeItem('user');
+    this.user.next(null);
   }
 
 }
